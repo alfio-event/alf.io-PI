82202e32b26019b191193478eb9de81270131f56
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-11-07 15:44:39 +0100

    implement #24

--- frontend/src/app/scan-module/scan/scan.service.ts
+++ frontend/src/app/scan-module/scan/scan.service.ts
@@ -14,6 +14,12 @@ export class ScanService {
         return this.performCheckIn(account, `${account.url}/admin/api/check-in/event/${eventKey}/ticket/${split[0]}`, scan);
     }
 
+    public forcePrintLabel(eventKey: string, account: Account, scan: string) {
+        let split = scan.split("/");
+        let url = `${account.url}/admin/api/check-in/event/${eventKey}/force-print-label-ticket/${split[0]}`;
+        return this.http.post(url, {"code" : scan});
+    }
+
     public confirmPayment(eventKey: string, account: Account, scan: string): Observable<TicketAndCheckInResult> {
         let split = scan.split("/");
         return this.performCheckIn(account, `${account.url}/admin/api/check-in/event/${eventKey}/ticket/${split[0]}/confirm-on-site-payment`, scan);
