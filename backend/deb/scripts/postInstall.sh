72a10eeabc4c875fef300c08aab13d4036e2245b
Celestino Bellone <celestino@swicket.io>
2019-01-16 22:21:54 +0100

    enable alfio.service

--- backend/deb/scripts/postInstall.sh
+++ backend/deb/scripts/postInstall.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "iw dev wlan0 set power_save off" | cat - /etc/rc.local > /etc/rc.local.alfio && mv /etc/rc.local.alfio /etc/rc.local
 chmod a+x /etc/rc.local
+systemctl enable alfio.service
