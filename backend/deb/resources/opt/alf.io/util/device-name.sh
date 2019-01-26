9e2aabc204a7f53d828a5da84044115c4ed6812e
Celestino Bellone <celestino@swicket.io>
2019-01-26 16:49:15 +0100

    use cksum instead of sum in order to avoid collisions

--- backend/deb/resources/opt/alf.io/util/device-name.sh
+++ backend/deb/resources/opt/alf.io/util/device-name.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#source: https://en.wikipedia.org/wiki/Base36#bash_implementation
-export value=`echo $1 | sum | sed -n 's/\([A-Z0-9a-z]\+\).*/\1/p' | sed -e 's/^[0]*//'`
-result=""
-base36="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-while true; do
-  result=${base36:((value%36)):1}${result}
-  if [[ $((value=${value}/36)) -eq 0 ]]; then
-    break
-  fi
-done
-echo ${result}
+
+# source https://boulderappsco.postach.io/post/convert-decimal-to-base-36-alpha-numeric-in-bash-linux
+
+function decimal_to_base36() {
+    BASE36=($(echo {0..9} {A..Z}));
+    arg1=$@;
+    export BC_LINE_LENGTH=0
+    for i in $(bc <<< "obase=36; $arg1"); do
+    echo -n ${BASE36[$(( 10#$i ))]}
+    done && echo
+}
+
+decimal_value=`echo $1 | cksum | sed -n 's/\([A-Z0-9a-z]\+\).*/\1/p'`
+decimal_to_base36 ${decimal_value}
--- backend/deb/resources/opt/alf.io/util/init-printer.sh
+++ backend/deb/resources/opt/alf.io/util/init-printer.sh
@@ -2,9 +2,9 @@
 device_name=$(@ALFIO_UTILS_DIR@/device-name.sh $1)
 
 if [[ "$2" == "ZD410" ]]; then
-    nohup @ALFIO_UTILS_DIR@/printers/ZD410.sh "$device_name" "$1" &
+    nohup @ALFIO_UTILS_DIR@/printers/ZD410.sh "$1" "$device_name" &
 elif [[ "$2" == "TX220" ]]; then
-    nohup @ALFIO_UTILS_DIR@/printers/TX220.sh "$device_name" "$1" &
+    nohup @ALFIO_UTILS_DIR@/printers/TX220.sh "$1" "$device_name" &
 else
-    nohup @ALFIO_UTILS_DIR@/printers/LW450T.sh "$device_name" "$1" &
+    nohup @ALFIO_UTILS_DIR@/printers/LW450T.sh "$1" "$device_name" &
 fi
