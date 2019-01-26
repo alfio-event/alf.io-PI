41afcc663358e2c5affe62ce97eedb03e3e68738
Celestino Bellone <celestino@swicket.io>
2019-01-26 21:31:48 +0100

    run printer-specific scripts using systemd-run

--- backend/deb/resources/opt/alf.io/util/init-printer.sh
+++ backend/deb/resources/opt/alf.io/util/init-printer.sh
@@ -11,12 +11,12 @@ device_name=$(@ALFIO_UTILS_DIR@/device-name.sh $1)
 
 case "$2" in
     "ZBR")
-        nohup @ALFIO_UTILS_DIR@/printers/ZD410.sh "$1" "Alfio-$2-$device_name" &
+        systemd-run --uid=pi --no-block --on-active=10 @ALFIO_UTILS_DIR@/printers/ZD410.sh "$1" "Alfio-$2-$device_name"
         ;;
     "BXL")
-        nohup @ALFIO_UTILS_DIR@/printers/TX220.sh "$1" "Alfio-$2-$device_name" &
+        systemd-run --uid=pi --no-block --on-active=10 @ALFIO_UTILS_DIR@/printers/TX220.sh "$1" "Alfio-$2-$device_name"
         ;;
     "DYM")
-        nohup @ALFIO_UTILS_DIR@/printers/LW450T.sh "$1" "Alfio-$2-$device_name" &
+        systemd-run --uid=pi --no-block --on-active=10 @ALFIO_UTILS_DIR@/printers/LW450T.sh "$1" "Alfio-$2-$device_name"
         ;;
 esac
--- backend/deb/resources/opt/alf.io/util/printers/ZD410.sh
+++ backend/deb/resources/opt/alf.io/util/printers/ZD410.sh
@@ -9,7 +9,7 @@
 # {name}: device name
 #
 /usr/sbin/lpadmin -E -p "$2" -v "usb://Zebra%20Technologies/ZTC%20ZD410-203dpi%20ZPL?serial=$1" -m drv:///sample.drv/zebra.ppd -o printer-error-policy=abort-job -o printer-is-shared=false
-su pi -c "/usr/bin/lpoptions -p $2 -o usb-unidir-default=true -o Darkness=30 -o zePrintRate=4"
+/usr/bin/lpoptions -p "$2" -o usb-unidir-default=true -o Darkness=30 -o zePrintRate=4
 /usr/sbin/cupsenable "$2"
 /usr/sbin/cupsaccept "$2"
-/sbin/rmmod usblp
+sudo /sbin/rmmod usblp
