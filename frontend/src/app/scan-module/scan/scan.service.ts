888162e0a7f360f957bd68888462a338cf5f9f05
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2025-10-07 10:29:29 +0200

    clean control codes from reading

--- frontend/src/app/scan-module/scan/scan.service.ts
+++ frontend/src/app/scan-module/scan/scan.service.ts
@@ -10,19 +10,22 @@ export class ScanService {
     }
 
     public checkIn(eventKey: string, account: Account, scan: string): Observable<TicketAndCheckInResult> {
-        let split = scan.split("/");
-        return this.performCheckIn(account, `${account.url}/admin/api/check-in/event/${eventKey}/ticket/${split[0]}`, scan);
+        let cleanScan = this.cleanScan(scan);
+        let split = cleanScan.split("/");
+        return this.performCheckIn(account, `${account.url}/admin/api/check-in/event/${eventKey}/ticket/${split[0]}`, cleanScan);
     }
 
     public forcePrintLabel(eventKey: string, account: Account, scan: string) {
-        let split = scan.split("/");
+        let cleanScan = this.cleanScan(scan);
+        let split = cleanScan.split("/");
         let url = `${account.url}/admin/api/check-in/event/${eventKey}/force-print-label-ticket/${split[0]}`;
-        return this.http.post(url, {"code" : scan});
+        return this.http.post(url, {"code" : cleanScan});
     }
 
     public confirmPayment(eventKey: string, account: Account, scan: string): Observable<TicketAndCheckInResult> {
-        let split = scan.split("/");
-        return this.performCheckIn(account, `${account.url}/admin/api/check-in/event/${eventKey}/ticket/${split[0]}/confirm-on-site-payment`, scan);
+        let cleanScan = this.cleanScan(scan);
+        let split = cleanScan.split("/");
+        return this.performCheckIn(account, `${account.url}/admin/api/check-in/event/${eventKey}/ticket/${split[0]}/confirm-on-site-payment`, cleanScan);
     }
 
     private performCheckIn(account: Account, url: string, scan: string): Observable<TicketAndCheckInResult> {
@@ -31,5 +34,23 @@ export class ScanService {
           code = null;
         }
         return this.http.post(url, {"code": code}).map(r => r.json());
+    }
+
+    private cleanScan(scan: string): string {
+      // remove any occurrence of \000026
+      const cleanScan = scan.replace(/\\000026/g,'');
+      const firstSlashIndex = cleanScan.indexOf("/");
+      if (firstSlashIndex === -1) {
+        return cleanScan; // code is not valid
+      }
+      let uuid = cleanScan.substring(0, firstSlashIndex);
+      const code = cleanScan.substring(firstSlashIndex+1);
+
+      if (uuid.length > 36) {
+        // code contains control chars
+        uuid = uuid.substring(uuid.length - 36);
+      }
+
+      return `${uuid}/${code}`;
     }
 }
